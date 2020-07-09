81a4faacac207c73cde7406157ff23460772b0e0
Siddharth Mishra <01siddharthmishra@gmail.com>
2020-07-09 11:18:07 +0530

    Update versions.tf

--- modules/mig_with_percent/versions.tf
+++ modules/mig_with_percent/versions.tf
@@ -15,9 +15,10 @@
  */
 
 terraform {
-  required_version = "~> 0.12.6"
   required_providers {
-    google      = ">= 2.7, <4.0"
-    google-beta = ">= 2.7, <4.0"
+    google = {
+      source = "hashicorp/google"
+    }
   }
+  required_version = ">= 0.13"
 }
